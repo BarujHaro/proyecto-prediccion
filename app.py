4b22176f0b8b9b5225c5caa4918b57f9066f96d2
BarujHaro <145818742+BarujHaro@users.noreply.github.com>
2025-04-28 15:21:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,4 @@ app.register_blueprint(views, url_prefix="/")
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
-   # serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=5000)
